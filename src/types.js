f625c997aa15ae40bd5bcf2fbcbdbfd39d12641c
Alexandre Borela <alexandre@borela.tech>
2017-01-19 13:30:03 -0200

    Enhance types and descriptions

--- src/types.js
+++ src/types.js
@@ -10,30 +10,99 @@
 // License for the specific language governing permissions and limitations under
 // the License.
 
+export type Profile = {
+  id:string,
+  name:string,
+  emails:Array<string>
+}
+
+export type SuccessData = {
+  name:string,
+  tokens:Object,
+  profile:Profile
+}
+
+export type DeniedData = {
+  provider:string
+}
+
+export type FailedData = {
+  provider:string,
+  error:string
+}
+
+export type SuccessCallback = (data:SuccessData) => void
+export type DeniedCallback = (data:DeniedData) => void
+export type FailedCallback = (data:FailedData) => void
+
+// TODO
 export type OAuth1aOptions = {
   consumerKey:string,
   consumerSecret:string
 }
 
+/**
+ * Any provider using the OAuth2 will require these options.
+ */
 export type OAuth2Options = {
   clientId:string,
   clientSecret:string,
-  scope:string|Arrayr<string>
+  scope?:string|Array<string>
 }
 
+// TODO
 export type LocalOptions = {}
 
+/**
+ * Available providers.
+ */
 export type ProvidersOptions =  {
   facebook?: OAuth2Options,
+  github?: OAuth2Options,
   google?: OAuth2Options,
+  linkedin?: OAuth2Options,
   local?: LocalOptions,
   twitter?: OAuth1aOptions,
   yahoo?: OAuth2Options
 }
 
+/**
+ * The middleware options.
+ */
 export type Options = {
+  /**
+   * The user was authenticated.
+   */
+  onSuccess:SuccessCallback,
+
+  /**
+   * The credentials were invalid or the user did not authorize access to the
+   * profile data.
+   */
+  onDenied:DeniedCallback,
+
+  /**
+   * Any other error.
+   */
+  onFailed:FailedCallback,
+
+  /**
+   * Domain required for the callback URL.
+   */
   domain:string,
+
+  /**
+   * Route used to initiate the authentication.
+   */
   authRoute:string,
+
+  /**
+   * Route used to process additional steps.
+   */
   callbackRoute:string,
+
+  /**
+   * Providers settings.
+   */
   providers:ProvidersOptions
 }
